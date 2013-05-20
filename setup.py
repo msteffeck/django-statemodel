5e333a62b9dbae311d2e4cf47097a92255d9c3c7
Michael Steffeck <masteffeck@gmail.com>
2013-05-20 15:03:17 -0700

    Added a setup file for pip install

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='django-statemodel',
+    version='1.0',
+    author='Michael Steffeck/Merchant Atlas Inc.',
+    author_email="msteffeck@merchantatlas.com",
+    url='https://github.com/MobileSpinach/django-statemodel',
+    license='3-clause BSD',
+    description="State management tool for Django models",
+    packages=("django_statemodel",),
+)
