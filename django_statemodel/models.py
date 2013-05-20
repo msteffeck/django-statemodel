18174785249e1f087bd9c71b86ffd1dde3ce113d
Michael Steffeck <masteffeck@gmail.com>
2013-05-20 13:34:21 -0700

    Created a mongodb version of the state model. Also reworked some things

--- django_statemodel/models.py
+++ django_statemodel/models.py
@@ -1,10 +1,8 @@
 from datetime import datetime
 from copy import copy
 
-from django.contrib.contenttypes import generic
-from django.contrib.contenttypes.models import ContentType
 from django.db import models
-from django.utils.functional import curry
+from djangotoolbox.fields import DictField, ListField
 
 from django_statemodel.signals import save_timestamp_cache, set_default_state
 
@@ -28,36 +26,6 @@ class StateModelBase(models.base.ModelBase):
         state_timestamps_field_name = options.get('state_timestamps_field_name',
                                                   'state_timestamps')
 
-        # state_map contains the mapping of states values to their attribute
-        # names.
-        state_map = options.get('state_map', [])
-        default_state = options.get('default_state')
-        add_states_to_model = options.get('add_states_to_model', True)
-
-        if state_map:
-            # default_state is either the first state in the map, or as
-            # overridden
-            if default_state is None:
-                default_state = state_map[0][0]
-
-            # Assign the states as attributes to the model
-            if add_states_to_model:
-                for key, value in state_map:
-                    attrs[value] = key
-
-        # db_index boolean to add an index to the state field. Defaults to True
-        db_index = options.get('db_index', True)
-
-        # Check if we should store the timestamp as utc
-        use_utc = options.get('use_utc', True)
-
-        # Check if we allow None states
-        allow_none_state = options.get('allow_none_state', True)
-        if not allow_none_state and default_state is None:
-            raise ValueError("'allow_none_state' cannot be False while "
-                             "'default_state' is set to None or 'state_map' "
-                             "is undefined.")
-
         # Get a Django field from the given model's _meta object
         def get_field(model, field):
             if hasattr(model, "_meta"):
@@ -70,48 +38,78 @@ class StateModelBase(models.base.ModelBase):
         # Check if any of the inherited models have the state field
         parent_has_state = False
         for parent in bases:
-            if bool(get_field(parent, state_field_name)):
+            if bool(get_field(parent, state_field_name)) or \
+                    bool(get_field(parent, state_timestamps_field_name)):
                 parent_has_state = True
                 break
+
+        # If the options are in the attrs and the state field isn't already
+        # inherited, add the statemodel fields. Django doesn't allow overriding.
+        if not parent_has_state and options:
+
+            # state_map contains the mapping of states values to their attribute
+            # names.
+            state_map = options.get('state_map', [])
+            default_state = options.get('default_state')
+            add_states_to_model = options.get('add_states_to_model', True)
+
+            if state_map:
+                # default_state is either the first state in the map, or as
+                # overridden
+                if default_state is None:
+                    default_state = state_map[0][0]
+
+                # Assign the states as attributes to the model
+                if add_states_to_model:
+                    for key, value in state_map:
+                        attrs[value] = key
+
+            # db_index boolean to add an index to the state field.
+            db_index = options.get('db_index', True)
+
+            # Check if we should store the timestamp as utc
+            use_utc = options.get('use_utc', True)
+
+            # Check if we allow None states
+            allow_none_state = options.get('allow_none_state', True)
+            if not allow_none_state and default_state is None:
+                raise ValueError("'allow_none_state' cannot be False while "
+                                 "'default_state' is set to None or 'state_map'"
+                                 " is undefined.")
+
+            # Add the state-model fields
+            attrs[state_field_name] = models.IntegerField(null=True,
+                                                          db_index=db_index,
+                                                          choices=state_map)
+            attrs[state_timestamps_field_name] = \
+                        DictField(ListField(models.DateTimeField()),
+                                  default={},
+                                  blank=True)
+
+            # Save the options for this model in an object attached to the model
+            options_cache = StateModelBase.StateModelOptions(
+                dict(state_map), default_state, use_utc, allow_none_state,
+                db_index, add_states_to_model, state_field_name,
+                state_timestamps_field_name)
+            attrs[OPTIONS_ATTR_NAME] = options_cache
+
+        elif parent_has_state and options:
+            raise AttributeError("Django does not allow field overriding with "
+                                 "inheritance. '%s' should only be a member "
+                                 "of one model in the inheritance tree."
+                                 % OPTIONS_CLASS)
+
+        cls = super(StateModelBase, mcs).__new__(mcs, name, bases, attrs)
 
         # Check if this is an abstract model
         is_abstract_model = getattr(attrs.get('Meta', {}), "abstract", False)
 
-        # If this model is abstract and the state field isn't inherited, add it
-        if is_abstract_model and not parent_has_state:
-            attrs[state_field_name] = models.IntegerField(null=True,
-                                                          db_index=db_index)
-            attrs[state_timestamps_field_name] = generic.GenericRelation(
-                                            StateTransitionTimestamp,
-                                            content_type_field='content_type',
-                                            object_id_field='content_id')
-
-        cls = super(StateModelBase, mcs).__new__(mcs, name, bases, attrs)
-
-        # Save the options for this model in an object attached to the model
-        options_cache = StateModelBase.StateModelOptions(
-                    dict(state_map), default_state, use_utc, allow_none_state,
-                    db_index, add_states_to_model, state_field_name,
-                    state_timestamps_field_name)
-        setattr(cls, OPTIONS_ATTR_NAME, options_cache)
-
         # Add signals to the inheriting models to save the state transitions
         if not is_abstract_model:
-            models.signals.post_save.connect(save_timestamp_cache,
-                                             sender=cls)
+            models.signals.pre_save.connect(save_timestamp_cache,
+                                            sender=cls)
             models.signals.post_init.connect(set_default_state,
                                              sender=cls)
-
-        state_field = get_field(cls, state_field_name)
-        if state_map and state_field:
-            # Set up the choices on the state field
-            state_field._choices = state_map
-
-            # Add in the django 'get_<field>_display' method. This is done
-            # in the django metaclass, which has run already, but needs choices
-            # to work.
-            setattr(cls, 'get_%s_display' % state_field.attname,
-                    curry(cls._get_FIELD_display, field=state_field))
         return cls
 
     class StateModelOptions(object):
@@ -127,36 +125,7 @@ class StateModelBase(models.base.ModelBase):
             self.state_field_name = state_field_name
             self.state_timestamps_field_name = state_timestamps_field_name
             self.state_timestamps_cache_name = \
-                                    "%s_cache" % state_timestamps_field_name
-
-
-class StateTransitionTimestamp(models.Model):
-    state = models.IntegerField(
-                blank=False,
-                null=False,
-                help_text="The state of this transition")
-
-    state_time = models.DateTimeField(
-                blank=False,
-                null=False,
-                default=datetime.utcnow,
-                help_text="The time this state was entered")
-
-    content_type = models.ForeignKey(
-                ContentType,
-                blank=True,
-                null=True)
-
-    content_id = models.PositiveIntegerField(
-                blank=False,
-                null=False)
-
-    content = generic.GenericForeignKey(
-                ct_field="content_type",
-                fk_field="content_id")
-
-    def __unicode__(self):
-        return "%s: %s" % (self.state, self.state_time)
+                                    "_%s_cache" % state_timestamps_field_name
 
 
 class StateModel(models.Model):
@@ -199,8 +168,6 @@ class StateModel(models.Model):
                 # We store the timestamp in a cache until the model is saved.
                 # This way, we only update the state_timestamps once per save.
                 setattr(self,
-                        meta_options.state_timestamps_cache_name,
-                        StateTransitionTimestamp(state=value,
-                                                 state_time=timestamp))
+                        meta_options.state_timestamps_cache_name, timestamp)
 
         super(StateModel, self).__setattr__(key, value)
--- django_statemodel/signals.py
+++ django_statemodel/signals.py
@@ -1,18 +1,38 @@
 
 def save_timestamp_cache(sender, instance, **kwargs):
-    """Save the StateTransitionTimestamp if there is one
+    """Save the State transition timestamp if there is one
 
-    post_save signal for statemodel.
+    pre_save signal for statemodel.
     """
-    from .statemodel import OPTIONS_ATTR_NAME
+    from .models import OPTIONS_ATTR_NAME
 
+    # Get the cache where the current state's timestamp was added
     meta_options = getattr(instance, OPTIONS_ATTR_NAME)
     state_timestamp_cache = getattr(instance,
                                     meta_options.state_timestamps_cache_name,
                                     None)
+
+    # If the cache is None, that means there were no state changes
     if state_timestamp_cache:
-        state_timestamp_cache.content = instance
-        state_timestamp_cache.save()
+        # Get the field where we store the state timestamps
+        state_timestamps = getattr(instance,
+                                   meta_options.state_timestamps_field_name)
+        # Get the state field value
+        state = str(getattr(instance, meta_options.state_field_name))
+        # The list of timestamps for a state
+        state_ts_list = state_timestamps.get(state)
+
+        # If the timestamp list is empty or missing, create it. Otherwise
+        # append the new timestamp to the list
+        if not isinstance(state_ts_list, list):
+            state_ts_list = [state_timestamp_cache]
+        else:
+            state_ts_list.append(state_timestamp_cache)
+
+        # Add the updated list back to the dict of states
+        state_timestamps[state] = state_ts_list
+        # Clear the cache
+        setattr(instance, meta_options.state_timestamps_cache_name, None)
 
 
 def set_default_state(sender, instance, **kwargs):
@@ -23,7 +43,7 @@ def set_default_state(sender, instance, **kwargs):
 
     post_init signal for statemodel.
     """
-    from .statemodel import OPTIONS_ATTR_NAME, DONE_INITIALIZING
+    from .models import OPTIONS_ATTR_NAME, DONE_INITIALIZING
 
     # Set the done_initializing flag to True. That means the __init__() method
     # is done running. This is used in __setattr__()
